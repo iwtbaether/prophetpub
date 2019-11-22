942854ac8012a6c6ff0a3d37c7c7aa682b28713d
iwtbaether <56656035+iwtbaether@users.noreply.github.com>
2019-11-22 01:49:28 -0800

    Add files via upload

---
+++ precache-manifest.840df8f1a3eb634a334111d73e89c8cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21fb4143604d40b54016ef2085f8a88d",
+    "url": "/prophet/index.html"
+  },
+  {
+    "revision": "141e1041ff9483f23281",
+    "url": "/prophet/static/css/main.d503a2df.chunk.css"
+  },
+  {
+    "revision": "3ea5eb6d5d41c555ea87",
+    "url": "/prophet/static/js/2.d987f969.chunk.js"
+  },
+  {
+    "revision": "141e1041ff9483f23281",
+    "url": "/prophet/static/js/main.8669cd54.chunk.js"
+  },
+  {
+    "revision": "04cfeabf75f5af70f445",
+    "url": "/prophet/static/js/runtime-main.1fcfd3b5.js"
+  }
+]);
