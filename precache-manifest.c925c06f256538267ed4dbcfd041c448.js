b6329d6dfb0e9512d7edd377dbed255a1099f216
iwtbaether <56656035+iwtbaether@users.noreply.github.com>
2019-11-22 02:03:34 -0800

    Add files via upload

---
+++ precache-manifest.c925c06f256538267ed4dbcfd041c448.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a9acb11a6a8e858cab4e622a59869b6",
+    "url": "/prophetpub/index.html"
+  },
+  {
+    "revision": "1e158a7737034028f1ef",
+    "url": "/prophetpub/static/css/main.d503a2df.chunk.css"
+  },
+  {
+    "revision": "3ea5eb6d5d41c555ea87",
+    "url": "/prophetpub/static/js/2.d987f969.chunk.js"
+  },
+  {
+    "revision": "1e158a7737034028f1ef",
+    "url": "/prophetpub/static/js/main.0a62af6a.chunk.js"
+  },
+  {
+    "revision": "677580c8a8ccfe9d84d5",
+    "url": "/prophetpub/static/js/runtime-main.c493fa43.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/prophet/precache-manifest.840df8f1a3eb634a334111d73e89c8cc.js"
+  "/prophetpub/precache-manifest.c925c06f256538267ed4dbcfd041c448.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/prophet/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/prophetpub/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
