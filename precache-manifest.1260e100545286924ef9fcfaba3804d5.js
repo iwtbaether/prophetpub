c708681067b421ff1c6310d58d6117c4c1d3d480
a <a>
2019-12-05 18:06:07 -0800

    idk

---
+++ precache-manifest.1260e100545286924ef9fcfaba3804d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0a124a765f5be43f08cf282289c14ab",
+    "url": "/prophetpub/index.html"
+  },
+  {
+    "revision": "ea43d3690cc8754fd741",
+    "url": "/prophetpub/static/css/main.2675fe6f.chunk.css"
+  },
+  {
+    "revision": "6272de805779183b2653",
+    "url": "/prophetpub/static/js/2.f0752ee6.chunk.js"
+  },
+  {
+    "revision": "ea43d3690cc8754fd741",
+    "url": "/prophetpub/static/js/main.79c472dd.chunk.js"
+  },
+  {
+    "revision": "677580c8a8ccfe9d84d5",
+    "url": "/prophetpub/static/js/runtime-main.c493fa43.js"
+  }
+]);
