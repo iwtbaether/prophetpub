f42c6e405b34f9fdf369c05e2d9ef218c8b7b351
a <a>
2020-02-15 05:30:14 -0800

    update

---
+++ precache-manifest.87a9379eced62e2e642420610e76761b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d65b1303019b460ca5e4192a4f582f49",
+    "url": "/prophetpub/index.html"
+  },
+  {
+    "revision": "0dc7b969ab19f77f10b6",
+    "url": "/prophetpub/static/css/main.af72d544.chunk.css"
+  },
+  {
+    "revision": "7ec9a861131c789d6a7f",
+    "url": "/prophetpub/static/js/2.6cc4f79a.chunk.js"
+  },
+  {
+    "revision": "0dc7b969ab19f77f10b6",
+    "url": "/prophetpub/static/js/main.24b4e5ed.chunk.js"
+  },
+  {
+    "revision": "677580c8a8ccfe9d84d5",
+    "url": "/prophetpub/static/js/runtime-main.c493fa43.js"
+  }
+]);
