6a75ae311260d5cf21ca875632a787e72d11c5ab
a <a>
2020-11-16 12:52:03 -0800

    have to use this i guess

---
+++ precache-manifest.7c4307a6c691b952c203b75eef6db2df.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2feb3e52457a2c55ddb719d010d4ba2",
+    "url": "/prophetpub/index.html"
+  },
+  {
+    "revision": "4e4058fd4aa5bb590f73",
+    "url": "/prophetpub/static/css/main.bab55db5.chunk.css"
+  },
+  {
+    "revision": "fd999d91c1c1ef5ee23e",
+    "url": "/prophetpub/static/js/2.4ff7cdf7.chunk.js"
+  },
+  {
+    "revision": "4e4058fd4aa5bb590f73",
+    "url": "/prophetpub/static/js/main.641aa211.chunk.js"
+  },
+  {
+    "revision": "5edc8f3883226c5ad145",
+    "url": "/prophetpub/static/js/runtime-main.8a1c7993.js"
+  }
+]);
